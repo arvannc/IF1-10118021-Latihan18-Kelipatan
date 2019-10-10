335a547201892bb86c1e01f0a9e54ec5fe22ca80
M Arvan Nurcahyana <55648161+arvannc@users.noreply.github.com>
2019-10-10 14:32:05 +0700

    Add files via upload

---
+++ src/if1/pkg10118021/latihan18/kelipatan/IF110118021Latihan18Kelipatan.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package if1.pkg10118021.latihan18.kelipatan;
+
+/**
+ *
+ * @author Arvan
+ * NAMA     : M Arvan Nurcahyana
+ * KELAS    : IF-1
+ * NIM      : 10118021
+ * Deskripsi Program : Kelipatan
+ */
+public class IF110118021Latihan18Kelipatan {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        for (double i = 3.5; i <= 35; i+=3.5)
+            System.out.println(i);
+        System.out.println("(Developed by : M. Arvan Nurcahyana)");
+    }
+    
+}
